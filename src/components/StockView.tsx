b33cbb8497b6d6be85652cff25fcd7910264182b
sixstring982 <sixstring982@gmail.com>
2016-06-09 10:34:57 -0600

    Average Buy line on chart
    Chart now plots Average Buy price when shares are owned.

--- src/components/StockView.tsx
+++ src/components/StockView.tsx
@@ -5,6 +5,7 @@ interface StockViewPropTypes {
     id: number,
     chartTitle: string,
     values: Array<number>,
+    buy_average: number,
 }
 
 export default class StockView extends React.Component<StockViewPropTypes, any> {
@@ -20,6 +21,30 @@ export default class StockView extends React.Component<StockViewPropTypes, any> 
         return <div id={`chart-${this.props.id}`}></div>;
     }
 
+    private generatePlotLines(): Array<Object> {
+        if (this.props.buy_average === null) {
+            return [];
+        }
+
+        const last_value = this.props.values[this.props.values.length - 1];
+
+        let average_line_color: string;
+        if (last_value === this.props.buy_average) {
+            average_line_color = 'gray';
+        } else if (last_value > this.props.buy_average) {
+            average_line_color = 'green';
+        } else {
+            average_line_color = 'red';
+        }
+
+        return [{
+            color: average_line_color,
+            dashStyle: 'longdash',
+            value: this.props.buy_average,
+            width: 2,
+        }];
+    }
+
     private renderChart() {
         const chartOptions = {
             chart: {
@@ -33,6 +58,7 @@ export default class StockView extends React.Component<StockViewPropTypes, any> 
                 title: {
                     text: 'Price ($)'
                 },
+                plotLines: this.generatePlotLines(),
             },
             series: [{
                 name: this.props.chartTitle,
--- src/containers/StockView.ts
+++ src/containers/StockView.ts
@@ -5,6 +5,7 @@ const mapStateToProps = state => ({
     id: 0,
     chartTitle: state.stox.getCurrentSymbol(),
     values: state.stox.getCurrentValues(),
+    buy_average: state.stox.getCurrentShareCount() > 0 ? state.stox.getCurrentBuyAverage() : null,
 });
 
 const mapDispatchToProps = dispatch => ({
